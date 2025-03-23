7def3361871fb0e58403adddb53b0180d495f9a4
AuraCat <136636937+AuraCat66@users.noreply.github.com>
2025-03-24 00:40:03 +0100

    add meta tags for other websites like Discord or Twitter

--- routes/_app.tsx
+++ routes/_app.tsx
@@ -5,8 +5,44 @@ export default function App({ Component }: PageProps) {
       <head>
         <meta charset="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <title>AuraCat's Personal Website</title>
         <link rel="stylesheet" href="/styles.css" />
+
+        {/* Primary Meta Tags */}
+        <title>AuraCat's Personal Website</title>
+        <meta name="title" content="AuraCat's Personal Website" />
+        <meta name="description" content="The personal website of AuraCat" />
+
+        {/* Open Graph / Facebook */}
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://auracat.deno.dev/" />
+        <meta property="og:title" content="AuraCat's Personal Website" />
+        <meta
+          property="og:description"
+          content="The personal website of AuraCat"
+        />
+        {
+          /* <meta
+          property="og:image"
+          content="https://metatags.io/images/meta-tags.png"
+        /> */
+        }
+
+        {/* Twitter */}
+        {/* <meta property="twitter:card" content="summary_large_image" /> */}
+        <meta property="twitter:url" content="https://auracat.deno.dev/" />
+        <meta property="twitter:title" content="AuraCat's Personal Website" />
+        <meta
+          property="twitter:description"
+          content="The personal website of AuraCat"
+        />
+        {
+          /* <meta
+          property="twitter:image"
+          content="https://metatags.io/images/meta-tags.png"
+        /> */
+        }
+
+        {/* Meta Tags Generated with https://metatags.io */}
       </head>
       <body>
         <Component />
