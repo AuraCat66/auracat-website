a758cf180e87b28e73e73b4f52a2d767a259f678
AuraCat <136636937+AuraCat66@users.noreply.github.com>
2025-03-24 15:34:38 +0100

    create a ReturnToPreviousPage island (button) and implement it in _404.tsx

--- routes/_404.tsx
+++ routes/_404.tsx
@@ -1,4 +1,5 @@
 import { Head } from "$fresh/runtime.ts";
+import ReturnToPreviousPage from "../islands/ReturnToPreviousPage.tsx";
 
 export default function Error404() {
   return (
@@ -9,6 +10,8 @@ export default function Error404() {
       <div class="text-center">
         <h1>404 - Page not found</h1>
         <h3>The page you were looking for doesn't exist.</h3>
+
+        <ReturnToPreviousPage></ReturnToPreviousPage>
       </div>
     </>
   );
