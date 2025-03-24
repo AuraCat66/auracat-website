0efc469936ef85835d83ed7754c58ac8e78ac883
AuraCat <136636937+AuraCat66@users.noreply.github.com>
2025-03-24 17:57:20 +0100

    removed useless !IS_BROWSER comparison in ReturnToPreviousPage island

--- islands/ReturnToPreviousPage.tsx
+++ islands/ReturnToPreviousPage.tsx
@@ -1,14 +1,10 @@
-import { IS_BROWSER } from "$fresh/runtime.ts";
-
 export default function ReturnToPreviousPage() {
   return (
-    !IS_BROWSER ? <div></div> : (
-      <button
-        type="button"
-        onClick={() => globalThis.history.back()}
-      >
-        Return to previous page
-      </button>
-    )
+    <button
+      type="button"
+      onClick={() => globalThis.history.back()}
+    >
+      Return to previous page
+    </button>
   );
 }
