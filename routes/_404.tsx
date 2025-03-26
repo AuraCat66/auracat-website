a6606dc7cad1a3fa04eb24290fadc5899d809731
AuraCat <136636937+AuraCat66@users.noreply.github.com>
2025-03-27 00:49:36 +0100

    make the h1 in _404 use PageTitle instead & make it red + remove unnecessary/unwanted div

--- routes/_404.tsx
+++ routes/_404.tsx
@@ -1,4 +1,5 @@
 import { Head } from "$fresh/runtime.ts";
+import PageTitle from "../components/PageTitle.tsx";
 import ReturnToPreviousPage from "../islands/ReturnToPreviousPage.tsx";
 
 export default function Error404() {
@@ -7,12 +8,10 @@ export default function Error404() {
       <Head>
         <title>404 - Page not found</title>
       </Head>
-      <div class="text-center">
-        <h1>404 - Page not found</h1>
-        <h3>The page you were looking for doesn't exist.</h3>
+      <PageTitle style="color: red;">404 - Page not found</PageTitle>
+      <h3>The page you were looking for doesn't exist.</h3>
 
-        <ReturnToPreviousPage></ReturnToPreviousPage>
-      </div>
+      <ReturnToPreviousPage></ReturnToPreviousPage>
     </>
   );
 }
