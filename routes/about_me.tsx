9cc0df4473c0dce7b1fad18d2e677543f6795250
AuraCat <136636937+AuraCat66@users.noreply.github.com>
2025-03-27 20:30:41 +0100

    create an actual about me page (WIP)

--- routes/about_me.tsx
+++ routes/about_me.tsx
@@ -4,7 +4,48 @@ export default function AboutMe() {
   return (
     <>
       <PageTitle>About me</PageTitle>
-      <h3>Possibly coming some day...</h3>
+      <h3 style="color: red;">(Work In Progress)</h3>
+      <p>
+        Hi! I am Laura, also known online as AuraCat, or Aura, or many other
+        nicknames.
+      </p>
+      <p>
+        I'm a 20+ year old queer trans woman from a cultural region of France
+        called{" "}
+        <a href="https://en.wikipedia.org/wiki/Northern_Catalonia">
+          "Northern Catalonia"
+        </a>, which mostly coincides with the French{" "}
+        <a href="https://en.wikipedia.org/wiki/Department_(administrative_division)">
+          department
+        </a>{" "}
+        of{" "}
+        <a href="https://en.wikipedia.org/wiki/Pyr%C3%A9n%C3%A9es-Orientales">
+          "Pyrénées-Orientales"
+        </a>.
+      </p>
+      <p>
+        I speak French (native lang), English, Spanish, Catalan, as well as some
+        Occitan.
+      </p>
+      <p>
+        I am a fierce advocate of marginalized languages/varieties and cultures.
+      </p>
+      <p>
+        I like programming, as well as languages and dialectology.
+      </p>
+      <p>
+        My main programming languages are{" "}
+        <a href="https://www.rust-lang.org/">Rust</a>, and{" "}
+        <a href="https://www.typescriptlang.org/">TypeScript</a>.
+      </p>
+      <p>
+        I don't have a lot of projects but I might make more over time, that
+        will be displayed in my <a href="/portfolio">portfolio</a>.
+      </p>
+      <p>
+        And by the way, I do not claim to be a very experimented or
+        professionnal programmer.
+      </p>
     </>
   );
 }
