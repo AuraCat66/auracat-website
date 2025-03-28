53b751227d05ba04921558bd745f761540a95b39
AuraCat <136636937+AuraCat66@users.noreply.github.com>
2025-03-28 17:08:15 +0100

    Fix NavigationPath for the home page

--- components/NavigationPath.tsx
+++ components/NavigationPath.tsx
@@ -5,33 +5,40 @@ export function NavigationPath(props: { url: URL }) {
   const pagePath = props.url.pathname.split("/");
 
   const elements: JSX.Element[] = [];
-
-  pagePath.map((e, i) => {
-    currentPath += `${e}/`;
+  if (pagePath[1] === "") {
     elements.push(
-      <>
-        {i < pagePath.length - 1
-          ? (
-            <>
-              <a
-                title={i > 0 ? e : "Home page"}
-                href={currentPath}
-              >
-                {i > 0 ? e : "Home"}
-              </a>
-              {" > "}
-            </>
-          )
-          : (
-            <span
-              title={e}
-            >
-              {e}
-            </span>
-          )}
-      </>,
+      <span title="Home page">
+        Home
+      </span>,
     );
-  });
+  } else {
+    pagePath.map((e, i) => {
+      currentPath += `${e}/`;
+      elements.push(
+        <>
+          {i < pagePath.length - 1
+            ? (
+              <>
+                <a
+                  title={i > 0 ? e : "Home page"}
+                  href={currentPath}
+                >
+                  {i > 0 ? e : "Home"}
+                </a>
+                {" > "}
+              </>
+            )
+            : (
+              <span
+                title={e}
+              >
+                {e}
+              </span>
+            )}
+        </>,
+      );
+    });
+  }
 
   return (
     <div class="navigation-path">
