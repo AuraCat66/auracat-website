40efd3a35b146c204487595e84faa719dc9fc1dd
AuraCat <136636937+AuraCat66@users.noreply.github.com>
2025-04-01 01:39:14 +0200

    add article previews to blog home page (finally)
    also make the "working on it" text red

--- routes/blog/index.tsx
+++ routes/blog/index.tsx
@@ -1,10 +1,17 @@
+import { generateArticlePreviews } from "../../blog/articleFns.tsx";
+import { flatTree } from "../../blog/index.ts";
 import PageTitle from "../../components/PageTitle.tsx";
 
 export default function Blog() {
+  const articles = flatTree.articleArray;
   return (
     <>
       <PageTitle>Blog</PageTitle>
-      <h3>Working on it 🏗️ ⚠️ 🚧 👷‍♀️</h3>
+      <h3 style={{ color: "red" }}>Working on it 🏗️ ⚠️ 🚧 👷‍♀️</h3>
+
+      <div class="text-left">
+        {generateArticlePreviews(articles)}
+      </div>
     </>
   );
 }
