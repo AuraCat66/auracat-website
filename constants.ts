3dc6e50072b91b56d8b1465613c3dbc41ad20d94
AuraCat <136636937+AuraCat66@users.noreply.github.com>
2025-04-01 01:56:20 +0200

    add amaranth (dark pink) color & make article preview bg and border pink

--- constants.ts
+++ constants.ts
@@ -2,6 +2,7 @@ export const colorPalette = {
   nonPhotoBlue: "#A7DCF1",
   vividSkyBlue: "#5bcefaff",
   cherryBlossomPink: "#f5a9b8ff",
+  amaranth: "#E5244B",
   mimiPink: "#fad4dcff",
   white: "#ffffffff",
 };
